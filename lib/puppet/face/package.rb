65861d3666491310c4c73eeca709763740a45de9
Carl Caum <carl@carlcaum.com>
2011-07-06 20:50:53 -0700

    Package Face and accompanying MCollective agent
    This is a fully functional package Face that provides a way to list all packages installed on a node, packages that have updates available, apply updates, and find installed package objects

---
+++ lib/puppet/face/package.rb
@@ -0,0 +1,107 @@
+require 'puppet/indirector/face'
+require 'puppet/package'
+
+Puppet::Indirector::Face.define(:package, '0.0.1') do
+  copyright "Puppet Labs", 2011
+  license   "Apache 2 license; see COPYING"
+
+  summary "View and manage packages on a node."
+  description <<-'EOT'
+    This subcommand interacts with package objects, using the default provider.
+    Upgrading multiple packages and viewing what updates are available for any
+    package type requires providers  that supports 'versionable' and 'upgradeable'.
+  EOT
+
+  save = get_action(:save).summary "Invalid for this face."
+  get_action(:search).summary "Invalid for this face."
+
+  destroy = get_action(:destroy)
+  destroy.summary "Uninstall a package object"
+  destroy.arguments "<package name>"
+  destroy.returns "A transaction report object"
+
+  find = get_action(:find)
+  find.summary "API only: Retrive a package object"
+  find.arguments "<package name>"
+  find.returns "A package object"
+
+
+  action :updates do
+    summary "List all packages with updates available from packaging system"
+    returns <<-'EOT'
+    A hash where the key is the package and the value is a hash containing `update`, 
+    `current`, and `provider`
+    EOT
+
+    when_invoked do  |options|
+      #We don't want the catalog.apply to print the logs to stdout
+      Puppet::Util::Log.close_all
+
+      Puppet::Package.find_updates
+    end
+
+    when_rendering :console do |packages|
+      output = Array.new
+
+      #Provide pretty output
+      layout = "\033[31m%-40s\033[32m%-30s\033[0m      \033[32m%-30s\033[0m%s"
+
+      output << layout % ["PACKAGE NAME", "CURRENT VERSION", "UPDATE AVAILABLE", "PROVIDER"]
+      output << '-'*120
+
+      output << packages.map do |name,package|
+        layout % [name, package[:current], package[:update], package[:provider] ]
+      end
+
+      output.flatten.join("\n")
+    end
+  end
+
+  action :update do
+    summary "Perform an update on a package"
+    arguments "<package_name> | <package hash>"
+    examples <<-EOT
+      When invoked from the command line:
+
+      $ puppet package update apr [--provider rpm] [--level 1.2.7-11.el5_6.5]
+
+      Or you can use in API form by passing in a hash of packages where each package value
+      contains a hash with keys `update` and `provider`:
+
+      updates = {
+        'apr'   => { :update => '1.2.7-11.el5_6.5', :provider => :rpm }
+        'stomp' => { :update => '1.1.6', :provider => :gem }
+      }
+
+      Puppet::Face[:package, '0.0.1'].update updates
+    EOT
+
+    Puppet::Package.add_update_options(self)
+
+    when_invoked do |package, options|
+      if package.class == Hash
+        Puppet::Package.apply_updates package
+      elsif package.class == String
+        Puppet::Package.update_package(package, options)
+      else
+        raise "Must give a Hash for String to update action"
+      end
+      nil #Don't render anything
+    end
+  end
+
+  action :list do
+    default
+    summary "List installed packages"
+
+    when_invoked do |options|
+      Puppet::Package.system_packages
+    end
+
+    when_rendering :console do |packages|
+      packages.map do |package|
+        package.name
+      end.join("\n")
+    end
+  end
+end
---
+++ lib/puppet/package.rb
@@ -0,0 +1,129 @@
+require 'puppet/indirector'
+require 'puppet/face'
+
+class Puppet::Package
+
+  # Set up indirection
+  extend Puppet::Indirector
+
+  indirects :package, :terminus_setting => :package_terminus
+
+  #Set our default indirector
+  # Am I doing this right?
+  Puppet.setdefaults(:main, :package_terminus => ["plain",'Where to get package information'])
+
+  def self.[](name)
+    system_packages.each do |package|
+      if package.name == name
+        return package
+      end
+    end
+  end
+
+  def self.add_update_options(action)
+    action.option '--level=', '-l=' do
+      summary "The version level to patch to. Default is latest"
+    end
+
+    action.option '--provider=', '-p=' do
+      summary "The provider for the package. Defaults to system default provider"
+    end
+  end
+
+  def self.system_packages
+    #Can't use Puppet::Resource face here
+    #Since it doesn't return package objects
+    Puppet::Type.type(:package).instances
+  end
+
+  #Takes in an array of Puppet::P
+  def self.apply_catalog(packages)
+    catalog = Puppet::Resource::Catalog.new
+
+    #According to the resources/catalog.rb file, you
+    #should be able to pass an array of resources
+    #to add_resource, but you can't, hmmm
+    packages.each do |package|
+      catalog.add_resource package
+    end
+
+    catalog.apply
+  end
+
+  #Takes a hash in the form
+  #  package_name = {
+  #    :update => <update_version>
+  #    :provider => <provider>
+  #  }
+  def self.apply_updates(packages)
+    packages.each do |name, package|
+      unless system_packages.map{ |p| p.name }.include? name
+        raise "#{name} is not installed on the system"
+      end
+
+      package_resource = Puppet::Resource.new( :package, "Package[#{name}]",
+        :parameters => {:ensure => package[:update], :provider => package[:provider]}
+      )
+
+      #Apply the update
+      begin
+        Puppet::Face[:resource,'0.0.1'].save package_resource
+      rescue => e
+        raise "Could not update \033[31m#{name}\033[0m: #{e}"
+      end
+    end
+  end
+
+  def self.update_package(package, options)
+    #The other methods expect the package name to be in the following format
+    level    = options.has_key?(:level) ? options[:level] : :latest
+    provider = options.has_key?(:provider) ? options[:provider] : Puppet::Type.type(:package).defaultprovider.name
+
+    #unless system_packages.map{ |p| p.name }.include? package
+      #raise ArgumentError, "#{package} is not installed on the system"
+    #end
+
+    #Perform the update
+    apply_updates package => { :update => level, :provider => provider }
+  end
+
+  def self.find_updates
+    package_updates = Hash.new
+
+    #Get our system packages, change our desired ensure to :latest
+    # and set it not to change anything when we apply the catalog
+    catalog_packages = system_packages.map do |package|
+      package[:ensure] = :latest
+      package[:noop]   = true
+
+      #The events in the reports from executing our manual built
+      #catalog won't give us the provider, so we need to save it now
+      package_updates[package.name] = {
+        :provider => package[:provider]
+      }
+
+      package #We need our array to contain the actual package objects
+    end
+
+    #Filter out packages which providers don't support upgradeable
+    ## Why won't select! work here?
+    catalog_packages.reject! { |package|  ! package.provider.upgradeable? }
+
+    report = apply_catalog(catalog_packages)
+    report.events.each do |update|
+      resource = update.instance_variable_get('@resource').split('[').last.chop
+
+      #The event doesn't show what the update version is
+      # so we have to extract it from the message
+      current_version, update_version = update.message.split(',')
+      current_version = current_version.split[-1]
+      update_version  = update_version.split[-2] # (noop) is the last element in the message
+
+      package_updates[resource][:current]  = current_version
+      package_updates[resource][:update]   = update_version
+    end
+
+    #Filter only the packages we found updates for
+    package_updates.reject{ |p,h| ! h.has_key?(:update) }
+  end
+end
